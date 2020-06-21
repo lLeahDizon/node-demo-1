f490aa9bc652cde728fe20b8330e89f37a4046e3
Lemon <516330797@qq.com>
2020-06-22 00:32:12 +0800

    完成作业

--- server.js
+++ server.js
@@ -26,7 +26,15 @@ var server = http.createServer(function (request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈\n`);
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+    <link rel="stylesheet" href="/x">
+    </head>
+    <body>
+        <h1>你好</h1>
+    </body>
+    `);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
@@ -36,7 +44,7 @@ var server = http.createServer(function (request, response) {
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`你输入的路径不存在对应的内容`);
+    response.write(`你访问的页面不存在`);
     response.end();
   }
 
